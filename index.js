281aae10d67adaf7728a9ade42c080ad8c7e987c
Minamaher-sw <mosadefmena185@gmail.com>
2025-06-08 20:13:17 +0300

    Refactor error handling middleware to utilize JSEND_STATUS for standardized error responses

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import dbConnect from "./src/config/db.connection.js";
 import userRouter from "./src/routes/user.route.js";
 import auth from "./src/middlewares/auth.middleware.js";
 import taskRouter from "./src/routes/task.route.js";
+import JSEND_STATUS from "./src/utils/http.status.message.js";
 
 const app =express();
 app.use(express.json());
@@ -25,15 +26,47 @@ app.listen(Port,hostname,() => {
 app.use("/user",userRouter)
 app.use(auth)
 app.use("/task",taskRouter)
+
+// wildcard route
+app.use("*",(req,res)=>{
+    res.status(404).json({
+        success: false,
+        message: "Route not found"
+    });
+})
 // error handler 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
-    console.error("Error:", err.message);
+    console.error("Error:", err);
+    let statusCode = err.statusCode !== 200 ? err.statusCode : 500;
+    let statusText = err.statusText || JSEND_STATUS.ERROR;
+    let message = err.message || "Internal Server Error";
 
-    const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+    //Validation Error from Mongoose Schema
+    if (err.name === "ValidationError") {
+        statusCode = 400;
+        statusText = JSEND_STATUS.FAIL;
+        const errors = Object.values(err.errors).map(e => e.message);
+        message = `Validation Error: ${errors.join(", ")}`;
+    }
+
+    // Cast Error (e.g. ObjectId or invalid date)
+    if (err.name === "CastError") {
+        statusCode = 400;
+        statusText = JSEND_STATUS.FAIL;
+        message = `Invalid ${err.path}: ${err.value}`;
+    }
+
+    // Duplicate key error from MongoDB
+    if (err.code && err.code === 11000) {
+        statusCode = 409;
+        statusText = JSEND_STATUS.FAIL;
+        const fields = Object.keys(err.keyValue).join(", ");
+        message = `Duplicate field value(s): ${fields}`;
+    }
 
     res.status(statusCode).json({
-        success: false,
-        message: err.message || "Internal Server Error",
+        status: statusText,
+        data: { message },
     });
 });
