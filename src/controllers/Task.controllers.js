48e22c59cc86d7f7677d007fdf961dcc3297913e
Minamaher-sw <mosadefmena185@gmail.com>
2025-06-08 20:14:01 +0300

    Refactor task controller methods to use asyncWrapper for improved error handling and response formatting

--- src/controllers/Task.controllers.js
+++ src/controllers/Task.controllers.js
@@ -1,93 +1,72 @@
+import asyncWrapper from "../middlewares/async.wrapper.js";
 import Task from "../models/task.model.js";
+import appErrors from "../utils/app.errors.js";
+import JSEND_STATUS from "../utils/http.status.message.js";
 
-const createTask =async(req,res)=>{
+import StatusCodes from "../utils/status.codes.js";
 
-    const TaskData =req.body ;
-    const user =req.user ;
-    const inserTask ={
-        title :TaskData.title,
-        description :TaskData.description ?TaskData.description  :" ",
-        dueDate:TaskData.dueDate,
-        priority:TaskData.priority ?TaskData.priority:"Low",
-        status :TaskData.status ?TaskData.status:"In Progress",
-        category:TaskData.category,
-        userID : user._id 
-    }
-    try{
-        const createdTask = await Task.create(inserTask);
-        res.status(201).json(createdTask);
-    }
-    catch(err){
-        res.status(500).json({message:err});
-    }
 
+const createTask = asyncWrapper(
+    async (req, res) => {
+        const user = req.user;
+        const createdTask = await Task.create({ userID: user._id, ...req.body });
+        res.status(StatusCodes.CREATED).json({ status: "success", data: createdTask });
+    }
+);
+
+const getAllTasks = asyncWrapper(
+    async (req, res) => {
+        const allTasks = await Task.find({ userID: req.user._id },{__v :false}).populate("userID");
+        res.status(StatusCodes.OK).json({ status: "success", data: allTasks });
 }
+)
 
-const getAllTasks = async (req,res)=>{
-
-    try{
-        const allTasks = await Task.find({userID: req.user._id }).populate("userID");
-        res.status(201).json(allTasks);
-    }
-    catch(err){
-        res.status(500).json({ message: err.message || err || "update is failed" });
-    }
-}
-
-const getTask = async (req,res)=>{
-
+const getTask = asyncWrapper(async (req, res) => {
     const taskId = req.params.id;
     const task = await Task.findOne({ _id: taskId, userID: req.user._id });
-    if (!task) return res.status(404).json({ message: "Task not found" });
-    try{
-        const retTask = await Task.findOne({_id:taskId});
-        res.status(201).json(retTask);
-    }
-    catch(err){
-        res.status(500).json({ message: err.message || err || "update is failed" });
-    }
-}
 
-const updateTask = async (req, res) => {
+    if (!task) 
+        { 
+            const error = appErrors.createError("Task not found",StatusCodes.NOT_FOUND,JSEND_STATUS.ERROR);
+            throw error ;
+        };
+    
+    const retTask = await Task.findOne({ _id: taskId });
+    res.status(StatusCodes.OK).json({ status: "success", data: retTask });
+})
+
+const updateTask = asyncWrapper(async (req, res) => {
     const taskId = req.params.id;
     const { title, description, priority, category, dueDate, status } = req.body;
-
-    try {
-
-        const task = await Task.findOne({ _id: taskId, userID: req.user._id });
-        if (!task) return res.status(404).json({ message: "Task not found" });
-
-        if (title !== undefined) task.title = title;
-        if (description !== undefined) task.description = description;
-        if (priority !== undefined) task.priority = priority;
-        if (category !== undefined) task.category = category;
-        if (dueDate !== undefined) task.dueDate = dueDate;
-        if (status !== undefined) task.status = status;
-
-        const updatedTask = await task.save();
-        res.status(200).json(updatedTask);
-        
-    } catch (err) {
-        if (err.name === "ValidationError") {
-            return res.status(400).json({ message: err.message });
-        }
-        res.status(500).json({ message: "Update failed", error: err.message || err });
-    }
-};
-
-const deleteTask = async (req,res)=>{
-    const taskId = req.params.id;
-
     const task = await Task.findOne({ _id: taskId, userID: req.user._id });
-    if (!task) return res.status(404).json({ message: "Task not found" });
-    try{
-        await Task.deleteOne({_id:taskId});
-        res.json({ message: "Task deleted successfully" })
+    if(!task){
+        const error = appErrors.createError("Task not found",StatusCodes.NOT_FOUND,JSEND_STATUS.ERROR);
+        throw error ;
     }
-    catch(err){
-        res.status(500).json({ message: err.message || err || "Task deleted failed" });
+    if (title !== undefined) task.title = title;
+    if (description !== undefined) task.description = description;
+    if (priority !== undefined) task.priority = priority;
+    if (category !== undefined) task.category = category;
+    if (dueDate !== undefined) task.dueDate = dueDate;
+    if (status !== undefined) task.status = status;
+
+    const updatedTask = await task.save();
+    res.status(StatusCodes.OK).json({ status: "success", data: updatedTask });
+})
+
+const deleteTask = asyncWrapper(
+    async (req, res) => {
+    const taskId = req.params.id;
+    const task = await Task.findOne({ _id: taskId, userID: req.user._id });
+    if (!task)
+    {
+        const error = appErrors.createError("Task not found",StatusCodes.NOT_FOUND,JSEND_STATUS.ERROR);
+        throw error ;
     }
+    await Task.deleteOne({ _id: taskId });
+    res.status(StatusCodes.OK).json({ status: "success", data: null });
 }
+)
 export{
     createTask, 
     getAllTasks ,
