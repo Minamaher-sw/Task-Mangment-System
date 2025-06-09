babf2e85ef122de4aba85f7bf644cb321d24251d
Minamaher-sw <mosadefmena185@gmail.com>
2025-06-09 14:06:13 +0300

    feat: enhance reminder scheduler to include web push notifications

--- src/utils/reminder.scheduler.js
+++ src/utils/reminder.scheduler.js
@@ -1,6 +1,7 @@
 import schedule from 'node-schedule'
 import sendReminderEmail from './email.notifications.js';
-
+import sendWebPushNotification from './web.push.js';
+import { getSubscription } from './subscriptionStore.js';
 const schedularReminder = (task) => {
     if (task.reminderTime) {
 
@@ -9,6 +10,9 @@ const schedularReminder = (task) => {
         schedule.scheduleJob(task._id.toString(), reminderDate2, () => {
             console.log(`Reminder for task ${task._id}: ${task.title} at ${task.reminderTime}`);
             sendReminderEmail(task);
+            const subscription = getSubscription()
+            console.log(subscription)
+            sendWebPushNotification(subscription ,task);
         });
     }
 };
