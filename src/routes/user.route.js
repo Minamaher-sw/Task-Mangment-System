9ca083453b14c683816374ebb71b6dcb3188b800
Minamaher-sw <mosadefmena185@gmail.com>
2025-11-28 00:22:37 +0200

    feat: add caching middleware to user retrieval route for improved performance

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,12 +1,13 @@
 import express from "express"
 import { getAllUser, signup ,userLogin ,getUserById ,updateUser ,deleteUser} from "../controllers/user.controllers.js"
+import { cacheMiddleware } from "../middlewares/cacheMiddleware.js";
 const userRouter =express.Router();
 
 
 userRouter.post("/",signup)
 userRouter.post("/login",userLogin)
 userRouter.get("/",getAllUser)
-userRouter.get("/:id",getUserById)
+userRouter.get("/:id",cacheMiddleware,getUserById)
 userRouter.patch("/:id",updateUser)
 userRouter.delete("/:id",deleteUser);
 
