33003890297491c0f18629d3e5b63633b6b890cc
Minamaher-sw <mosadefmena185@gmail.com>
2025-11-28 00:26:34 +0200

    feat: update schedularReminder to fetch user details and send email notifications

--- src/utils/reminder.scheduler.js
+++ src/utils/reminder.scheduler.js
@@ -2,14 +2,23 @@ import schedule from 'node-schedule'
 import sendReminderEmail from './email.notifications.js';
 import sendWebPushNotification from './web.push.js';
 import { getSubscription } from './subscriptionStore.js';
-const schedularReminder = (task) => {
+import User from '../models/user.model.js';
+const schedularReminder = async (task) => {
     if (task.reminderTime) {
-
+        const user = await User.findOne({_id:task.userID});
+            if (!user) {
+                console.error("User not found for task:", task._id);
+                return;
+            }
         console.log("Scheduling reminder for task:", task.title);
-        const reminderDate2 = new Date(task.reminderTime);
-        schedule.scheduleJob(task._id.toString(), reminderDate2, () => {
-            console.log(`Reminder for task ${task._id}: ${task.title} at ${task.reminderTime}`);
-            sendReminderEmail(task);
+        // Schedule the reminder using node-schedule
+
+        const utcDate = new Date(task.reminderTime);
+
+        const reminderTime = new Date(utcDate.getTime() - 3 * 60 * 60 * 1000);
+        schedule.scheduleJob(task._id.toString(), reminderTime, () => {
+            console.log(`Reminder for task ${task._id}: ${task.Title} at ${task.reminderTime}`);
+            sendReminderEmail(task,user.email);
             const subscription = getSubscription()
             console.log(subscription)
             sendWebPushNotification(subscription ,task);
